b0c1fb5557e2f86e99fe6f83f0e421e9d488282f
michael-duke <michael.21duke@gmail.com>
2022-11-22 18:30:34 +0300

    Implement custom methods for User, Post, Comment, and Like models Corrected Rubocop linter errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,16 @@
 class Post < ApplicationRecord
+  after_save :update_posts_counter
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+
+  def recent_five
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  private
+
+  def update_posts_counter
+    user.update(posts_counter: user.posts.all.length)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  def recent_three
+    posts.order(created_at: :desc).limit(3)
+  end
 end
