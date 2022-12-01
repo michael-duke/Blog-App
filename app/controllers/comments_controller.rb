73b66a05bc782d4569fff2cd14f8256a226fe5eb
michael-duke <michael.21duke@gmail.com>
2022-12-01 20:15:59 +0300

    Implement #new method and pass the Comment object as well as the current Post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,9 @@
 class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+    @post = Post.find(params[:post_id])
+  end
+
   def create
     post = Post.find(params[:post_id])
     comment = Comment.create(author: current_user, post:, **comment_params)
