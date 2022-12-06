96e1985680d6ca692cee19df7c3fd6bcb26e4a19
michael-duke <michael.21duke@gmail.com>
2022-12-06 17:16:17 +0300

    Generate devise model for User and configure Devise controller route Add default user photo, an avatar GIF Remove Devise mailer settings, since it was conflicting with seeding Run Migrations after devise has been added Seed database with new values taking regards of password, and password_confirmation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,12 +53,6 @@ Rails.application.configure do
   # Default URL options for the Devise mailer 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   
-  # Delivery method for the Devise mailer
-  config.action_mailer.delivery_method = :letter_opener
-
-  # Allow Devise to perform deliveries
-  config.action_mailer.perform_deliveries = true
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
