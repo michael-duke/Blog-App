422e45da0bea279d9f8a21afa1e566ee3a6e00f6
Amtresu <conor.follette@gmail.com>
2022-12-08 06:38:00 +0900

    Configured comments controller for authorization

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,21 +3,33 @@ class CommentsController < ApplicationController
     @comment = Comment.new
     @post = Post.find(params[:post_id])
   end
-
+​
   def create
     post = Post.find(params[:post_id])
     comment = Comment.create(author: current_user, post:, **comment_params)
-
+​
     if comment.save
       flash[:success] = 'Comment was successfully saved !'
     else
       flash.now[:error] = 'Error: Comment could not be saved'
     end
-    redirect_to user_post_url(current_user.id, post.id)
+    redirect_to user_post_url(post.author, post)
   end
-
+​
+  def destroy
+    user = User.find(params[:user_id])
+    post = user.posts.find(params[:post_id])
+    comment = Comment.find(params[:id])
+    if comment.destroy
+      flash[:success] = 'Comment was successfully deleted.'
+    else
+      flash[:error] = 'Error: Comment could not be deleted'
+    end
+    redirect_to user_post_path(user, post)
+  end
+​
   private
-
+​
   def comment_params
     params.require(:comment).permit(:text)
   end
