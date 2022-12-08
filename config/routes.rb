a2797dd40834462871b3e44461899bc993e8776a
michael-duke <michael.21duke@gmail.com>
2022-12-09 00:03:21 +0300

    Add routes for api endpoints with regards to namespace api/v1

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,16 @@ Rails.application.routes.draw do
       resources :likes, only: [:create]
     end
   end
+  
+  # API routes
+  namespace :api do
+    namespace :v1 do
+      resources :users do 
+        resources :posts do 
+          resources :comments
+          resources :likes
+        end
+      end
+    end
+  end
 end
