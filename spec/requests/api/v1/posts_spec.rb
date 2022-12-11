ec33b59c58c5d1dcc685f736a09a5ee0694559a0
michael-duke <michael.21duke@gmail.com>
2022-12-11 04:16:54 +0300

    Include the host configuration in the test environment for allowing the tests to run properly. Implement the tests for the API documentation using the swagger-docs gem.

--- spec/requests/api/v1/posts_spec.rb
+++ spec/requests/api/v1/posts_spec.rb
@@ -1,43 +1,49 @@
 require 'swagger_helper'
 
 RSpec.describe 'api/v1/posts', type: :request do
-  path "/api/v1/users/{user_id}/posts?access_token=#{@api_key.access_token}" do
-    get('list posts') do
-      produces 'application/json', 'application/xml'
-      parameter name: 'user_id', in: :path, type: :string, description: 'user_id'
-      parameter name: 'access_token', in: :path, type: :string, description: 'access_token'
-      response(200, 'successful') do
-        schema type: :object,
-               properties: {
-                 id: { type: :integer },
-                 title: { type: :string },
-                 content: { type: :string }
-               },
-               required: %w[id title content]
-        let(:user_id) { @author.id }
+  path '/api/v1/users/{user_id}/posts?access_token={access_token}' do
+    get 'List all posts for a User' do
+      tags 'Posts', 'All Posts'
+      produces 'application/json'
+      parameter name: :user_id, in: :path, type: :string, description: 'Author ID'
+      parameter name: :access_token, in: :path, type: :string, description: 'Authentication token'
+
+      response '200', 'successful' do
+        let(:user_id) { User.create(name: 'Syril Karn', photo: 'https://lumiere-a.akamaihd.net/v1/images/syril-karn-main-a_da01439f.jpeg?region=0%2C51%2C1920%2C960', bio: 'Syril Karn believes wholeheartedly in order and justice. The ambitious inspector hopes to make a name for himself within the ranks of the Empire by quelling signs of rebellious activity and exposing lawbreakers.', email: 'syrilkarn@empire.com', password: 'syrilkarn', password_confirmation: 'syrilkarn').id }
+        let(:access_token) { ApiKey.create.access_token }
+        run_test!
+      end
+
+      response '404', 'posts not found' do
+        let(:user_id) { 'invalid' }
+        let(:access_token) { ApiKey.create.access_token }
         run_test!
       end
     end
   end
 
-  path "/api/v1/users/{user_id}/posts/{id}?access_token=#{@api_key.access_token}" do
-    # You'll want to customize the parameter types...
-    parameter name: 'user_id', in: :path, type: :string, description: 'user_id'
-    parameter name: 'id', in: :path, type: :string, description: 'id'
-    parameter name: 'access_token', in: :path, type: :string, description: 'access_token'
+  path '/api/v1/users/{user_id}/posts/{id}?access_token={access_token}' do
+    get 'Show single post of User' do
+      tags 'Posts', 'Single Post'
+      produces 'application/json'
+      parameter name: :user_id, in: :path, type: :string, description: 'Author ID'
+      parameter name: :id, in: :path, type: :string, description: 'Post ID'
+      parameter name: :access_token, in: :path, type: :string, description: 'Authentication token'
 
-    get('show post') do
-      response(200, 'successful') do
-        let(:user_id) { '1' }
-        let(:id) { '1' }
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
+      response '200', 'post found' do
+        let(:user_id) { User.create(name: 'Bix Caleen', photo: 'https://lumiere-a.akamaihd.net/v1/images/bix-caleen-main-b_1450fe71.jpeg?region=103%2C0%2C1427%2C803&width=960', bio: 'Bix Caleen was a human female and mechanic who was the owner of the Caleen Salyard on the Free Trade sector planet of Ferrix. She worked alongside Timm Karlo, who was also her lover. She was close with her ex-boyfriend, Cassian Andor, who trusted her greatly. She also had offworld connections, namely Luthen Rael.', email: 'bixcaleen@rebels.com', password: 'bixcaleen', password_confirmation: 'bixcaleen').id }
+        let(:id) do
+          Post.create(author_id: user_id, title: 'Keep fighting the Empire',
+                      text: 'We are close to destroying the Death Star, all rebels come full out').id
         end
+        let(:access_token) { ApiKey.create.access_token }
+        run_test!
+      end
+
+      response '404', 'post not found' do
+        let(:user_id) { User.create(name: 'Bix Caleen', photo: 'https://lumiere-a.akamaihd.net/v1/images/bix-caleen-main-b_1450fe71.jpeg?region=103%2C0%2C1427%2C803&width=960', bio: 'Bix Caleen was a human female and mechanic who was the owner of the Caleen Salyard on the Free Trade sector planet of Ferrix. She worked alongside Timm Karlo, who was also her lover. She was close with her ex-boyfriend, Cassian Andor, who trusted her greatly. She also had offworld connections, namely Luthen Rael.', email: 'bixcaleen@rebels.com', password: 'bixcaleen', password_confirmation: 'bixcaleen').id }
+        let(:id) { 'invalid' }
+        let(:access_token) { ApiKey.create.access_token }
         run_test!
       end
     end
